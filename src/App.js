8eeb39927027f9850e0679c45eb8759993b69c0d
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2021-09-30 02:53:11 +0300

    1й, 2й, шаги

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+// import './App.css';
+import React from "react";
+import Container from "./components/Container";
+import Section from "./components/Section";
+import ContactForm from "./components/ContactForm";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Привет! Это настроенная версия приложения для работы с React</h2>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],    
+  };
+
+  formSubmitHandler = data => {
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, data]}
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.formSubmitHandler}>
+          </ContactForm>
+        </Section>
+        <Section title="Contacts">
+          <ul>
+            {this.state.contacts.map( contact => {
+              return (
+                <li key={contact.id}>{contact.name}: {contact.number}</li>
+              )
+            })}
+          </ul>
+        </Section>
+      </Container>
+    );
+  }
+};
 
 export default App;
