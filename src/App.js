84f8b28ae2c7ee4bdab786611a069392b0110183
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2021-09-29 15:16:18 +0300

    add react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Привет! Это настроенная версия приложения для работы с React</h2>
+    </div>
+  );
+}
+
+export default App;
